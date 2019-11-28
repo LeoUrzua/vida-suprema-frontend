8827e5ab1f2b7d23272dd2b93750bb46cc96c6df
Leonardo Urzua <d.leonardo.urzua@gmail.com>
2019-11-28 14:29:03 -0600

    chore: add basic typescript structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,26 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import logo from './logo.svg';
 import './App.css';
-import NavBar from './NavBar';
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import ArticlePage from "./pages/ArticlePage";
-import NotFoundPage from "./pages/NotFoundPage";
-import ArticlesListPage from "./pages/ArticlesListPage";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div className='App'>
-        <NavBar />
-        <div id='page-body'>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/articles-list" component={ArticlesListPage} />
-            <Route exact path="/article/:name" component={ArticlePage} />
-            <Route exact path="/about" component={AboutPage} />
-            <Route component={NotFoundPage} />
-          </Switch>
-        </div>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-}
+};
 
 export default App;
