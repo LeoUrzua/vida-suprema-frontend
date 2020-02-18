b5bfe4e7b22c56ee10366f156963f795fa505dc3
Leonardo Urzua <d.leonardo.urzua@gmail.com>
2020-02-18 15:44:28 -0600

    chore: refactor

--- src/utils/access-decision-manager-react/src/access-decision-manager-provider.tsx
+++ src/utils/access-decision-manager-react/src/access-decision-manager-provider.tsx
@@ -1,21 +1,19 @@
-/* eslint no-use-before-define: 0 */  // --> OFF
 import React, {createContext, useContext} from 'react';
 import AccessDecisionManager, {Voter} from '@wizeline/access-decision-manager/lib';
 
 
 type ADMContextType = {
-    isGranted: (attribute: string, subject: any)=> Boolean;
+    isGranted: (attribute: string, subject: any)=> Promise<Boolean>;
 }
 
 const admContext = createContext<ADMContextType>({
     isGranted(){
-        return false;
+        return Promise.resolve(false);
     }
     }
 );
 
-// @ts-ignore
-export const ProvideADM = ({voters, children}) => {
+export const ProvideADM = ({voters, children}: {voters: Voter[], children: any}) => {
     const accessDecisionManager = new AccessDecisionManager(null, voters, null);
     const adm = useProvideADM(accessDecisionManager);
     return <admContext.Provider value={adm}>{children}</admContext.Provider>
@@ -27,10 +25,9 @@ export const useADM = () => {
 
 const useProvideADM = (accessDecisionManager: AccessDecisionManager) => {
 
-    const isGranted = (attribute:any, subject:any) => {
-        const isGranted = accessDecisionManager.isGranted(attribute, subject);
-        console.log('result: ', isGranted);
-        return false;
+    const isGranted = async (attribute:any, subject:any) : Promise<Boolean> => {
+        const isGranted = await accessDecisionManager.isGranted(attribute, subject);
+        return isGranted;
     };
 
     return {
